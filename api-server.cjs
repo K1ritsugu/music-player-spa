44be0d36e4280bf07d3345cd8b6a95827ca71ec5
Juonior <gogame37@gmail.com>
2025-06-04 12:08:08 +0200

    add cors header for all request api

--- api-server.cjs
+++ api-server.cjs
@@ -74,13 +74,15 @@ function sendJSON(res, statusCode, data) {
 }
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
-    res.writeHead(200);
+    res.writeHead(204);
     res.end();
     return;
   }
-
   const parsedUrl = url.parse(req.url, true);
   const pathname = parsedUrl.pathname;
 
